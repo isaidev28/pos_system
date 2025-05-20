0bacea7f06e4e769a3f36541769023672dc0708f
isaidev28 <sisai2168@gmail.com>
2025-05-19 19:20:17 -0600

    feat: creacion del modelo de usuario personalizado con roles

--- ventas/models.py
+++ ventas/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('vendedor', 'Vendedor'),
+    )
+    
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='cliente')
+    
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
